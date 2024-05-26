c47ffb47b03bb2a061d8893c030706ebef00e615
daniel12142007 <danijelahatzanov@gmail.com>
2024-05-26 17:14:44 +0600

    Category(ADMN)(11) task completed successfully

---
+++ src/main/java/com/example/neftchi/api/admin/CategoryAdminApi.java
@@ -0,0 +1,41 @@
+package com.example.neftchi.api.admin;
+
+import com.example.neftchi.dto.response.CategoryResponse;
+import com.example.neftchi.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/category/admin")
+public class CategoryAdminApi {
+    private final CategoryService categoryService;
+
+    @GetMapping("find/all")
+    public List<CategoryResponse> findAll() {
+        return categoryService.findAll();
+    }
+
+    @PostMapping("save")
+    public List<CategoryResponse> save(@RequestParam String category,
+                                       @RequestParam String color) {
+        return categoryService.save(category, color);
+    }
+
+    @PutMapping("lowest/{id}")
+    public List<CategoryResponse> lowest(@PathVariable Long id) {
+        return categoryService.queueNumberLowest(id);
+    }
+
+    @PutMapping("higher/{id}")
+    public List<CategoryResponse> higher(@PathVariable Long id) {
+        return categoryService.queueNumberHigher(id);
+    }
+
+    @DeleteMapping("delete/last/queue/number")
+    public List<CategoryResponse> delete() {
+        return categoryService.deleteLastQueue();
+    }
+}
--- src/main/java/com/example/neftchi/repository/CategoryRepository.java
+++ src/main/java/com/example/neftchi/repository/CategoryRepository.java
@@ -1,12 +1,31 @@
 package com.example.neftchi.repository;
 
+import com.example.neftchi.dto.response.CategoryResponse;
 import com.example.neftchi.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Query("""
+            select
+            new com.example.neftchi.dto.response.CategoryResponse(
+            c.id,
+            c.category,
+            c.color,
+            c.queueNumber
+            )
+            from Category c
+            order by c.queueNumber
+            """)
+    List<CategoryResponse> findAllResponse();
+
+    @Query("select c from Category c where c.queueNumber = :queueNumber")
+    Category findByQueueNumber(@Param(value = "queueNumber") int queueNumber);
 }
